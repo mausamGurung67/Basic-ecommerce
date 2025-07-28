e2f1628891f7310ff26303a6eac119918b04c643
mausamGurung67 <mausamgurung678@gmail.com>
2025-07-28 10:10:50 +0545

    added privacy in the api secret keys

--- Backend/src/cofig/cloudinary.js
+++ Backend/src/cofig/cloudinary.js
@@ -1,12 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
+import constant from "./constant.js";
 
 // Configuration
 cloudinary.config({
-  cloud_name: "dronpb7f1",
-  api_key: "638227534456343",
-  api_secret: "oX00jYEFqQIZFMN4Ix_S3W1aCD8", // Click 'View API Keys' above to copy your API secret
+  cloud_name: constant.CLOUDINARY_CLOUD_NAME, // Click 'View API Keys' above to copy your cloud name
+  api_key: constant.CLOUDINARY_API_KEY, // Click 'View API Keys' above
+  api_secret: constant.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
 const storage = new CloudinaryStorage({
