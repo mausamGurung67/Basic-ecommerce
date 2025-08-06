369618b801e8f583bac86037421e9aaa3770e4d6
mausamGurung67 <mausamgurung678@gmail.com>
2025-08-06 12:11:09 +0545

    added multi-step verification logic with token, email and OTP verification

--- Backend/src/routes/authRoute.js
+++ Backend/src/routes/authRoute.js
@@ -5,13 +5,13 @@ import Otp from '../models/Otp.js'
 import { sendMail } from '../utils/sendMail.js'
 import User from '../models/User.js'
 import bcrypt from "bcrypt"
+import { verifyToken } from '../helper/token.js'
 
 const router = express.Router()
 
 router.post ('/register',register)
 router.post ('/login',login)
-// router.post ('/forgotPassword', forgotPassword)
-router.post ('/forgotPassword', forgotPassword)
+router.post ('/forgot-Password', forgotPassword)
 router.post("/verify-otp", verifyOtp)
 router.post("/reset-password", async(req,res) => {
     try{
@@ -50,7 +50,7 @@ router.post("/reset-password", async(req,res) => {
 
     } catch (error){
         console.log(error.message) 
-        res.send(error.message)
+        res.status(400).send(error.message)
     }
 })
 
@@ -64,30 +64,55 @@ router.get("/get-all-otps", async (req, res) => {
   }
 });
 
-router.get("/verify/:step", async(req,res) => {
+router.get("/verify/:step", async (req, res) => {
+  try {
+    const { step } = req.params;
+    const userEmail = req.cookies.userEmail;
+    const authToken = req.cookies.authToken;
 
-try{
-    const {step} = req.params;
+    console.log("Step:", step);
 
-    if(step ==="1"){
+    if (step === "1") {
+      console.log(authToken);
+      if (!authToken) {
+        throw new Error("Authentication token is missing");
+      }
 
+      const isValid = verifyToken(authToken);
+      if (!isValid) {
+        res.clearCookie("authToken");
+        throw new Error("Invalid authentication token");
+      }
+    } 
+
+    if (step === "2") {
+      if (!userEmail) {
+        throw new Error("User email is missing");
+      }
+
+      const isUserValid = await User.findOne({ email: userEmail });
+
+      if (!isUserValid) {
+        res.clearCookie("userEmail");
+        throw new Error("User not registered");
+      }
     }
 
-    if(step ==="2"){
+    if (step === "3") {
+      const isOtpVerified = await User.findOne({ email: userEmail });
 
+      if (new Date() > isOtpVerified.otpExpiresAt) {
+        throw new Error("OTP expired");
+      }
     }
 
-    if(step ==="3"){
-
-    }
-
-    res.status(200).json({message: `Verification step ${step} passed` })
-} catch (error) {
+    res.status(200).json({ message: `Verification step ${step} passed` });
+  } catch (error) {
     console.log(error);
-    res.status(400).json({error: error.message});
-}
+    res.status(400).json({ error: error.message });
+  }
+});
 
-})
 
 
 export default router
